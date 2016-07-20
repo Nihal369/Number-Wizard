1276d8661e45b42c20d34274d8fcd803075807f2
Nihal Ismail <nihalismailk@gmail.com>
2016-07-20 15:01:12 +0530

    Added random number generator

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
-	int max=1000;
-	int min=1;
-	int guess=500;
+	//Global variables
+	int max,min,guess;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +14,7 @@ public class NumberWizard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Keydown indicates key is pressed
 		if(Input.GetKeyDown("up"))
 		{
 			//print("Up key pressed");
@@ -41,8 +41,9 @@ public class NumberWizard : MonoBehaviour {
 	{
 		max=1000;
 		min=1;
-		guess=500;
-		print ("=============================");
+		//Random number generator of unity
+		guess=Random.Range(1,1001);
+		print ("=======================================");
 		print ("Welcome to Mathematic Horcrux");
 		print("Pick a number between 1 and 1000,but dont tell me");
 		
