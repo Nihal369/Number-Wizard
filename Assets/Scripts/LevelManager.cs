75c33ca394a58c14cca709db225c8802bb9d25ec
Nihal Ismail <nihalismailk@gmail.com>
2016-07-21 18:38:28 +0530

    Added android apk file

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,5 +5,15 @@ public class LevelManager : MonoBehaviour {
 	public void loadLevel(string level)
 	{
 		print("Request to load level "+level);
+		if(level=="Game")
+		{
+			Application.LoadLevel(level);
+		}
+	}
+	
+	public void quitGame()
+	{
+		print ("Quiting game");
+		Application.Quit();
 	}
 }
---
+++ Assets/Scripts/LevelManager2.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager2 : MonoBehaviour {
+	public void changeLevel(string level)
+	{
+		print("Request to load level");
+		Application.LoadLevel(level);
+	}
+}
