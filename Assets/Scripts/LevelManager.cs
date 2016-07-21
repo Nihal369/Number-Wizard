cad38eb70c69e85b531d9e68833c761aa0635f95
Nihal Ismail <nihalismailk@gmail.com>
2016-07-21 18:01:38 +0530

    Added OnClick to buttons

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void loadLevel(string level)
+	{
+		print("Request to load level "+level);
+	}
+}
